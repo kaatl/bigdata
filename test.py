076108195955c4286c3da8ebf6e6d95375b0a873
codden <caroline@codden.no>
2017-03-02 12:04:15 +0100

    added csv file to git-lfs

--- test.py
+++ test.py
@@ -7,9 +7,8 @@ if __name__ == "__main__":
     sc = SparkContext(appName="Test")
     sqlContext = SQLContext(sc)
 
-    # words = sc.textFile("listings_us.csv")
-
-    words = sc.textFile("test1.txt")
+    words = sc.textFile("listings_us.csv")
+    #words = sc.textFile("test1.txt")
     print("WORD COUNT: ", words.count())
 
     sc.stop()
