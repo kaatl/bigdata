cc4c8c23f18f286e86eb7c90356298119b268b94
codden <caroline@codden.no>
2017-03-02 11:50:40 +0100

    added main method

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 from pyspark import SparkContext
 from pyspark import SQLContext
 
-sc = SparkContext(appName="Test")
-sqlContext = SQLContext(sc)
 
-words = sc.textFile("/Users/Kathrine/Desktop/test1.txt")
-words.count()
-print(words.count())
+
+if __name__ == "__main__":
+    sc = SparkContext(appName="Test")
+    sqlContext = SQLContext(sc)
+
+    # words = sc.textFile("listings_us.csv")
+
+    words = sc.textFile("test1.txt")
+    print("WORD COUNT: ", words.count())
+
+    sc.stop()
