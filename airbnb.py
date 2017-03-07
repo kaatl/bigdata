9cb5988550c52ac1093d270bdd671bb07ac30c49
codden <caroline@codden.no>
2017-03-07 17:16:37 +0100

    added some prettyprint, and remove some spark-logging

--- airbnb.py
+++ airbnb.py
@@ -1,27 +1,34 @@
 from pyspark import SparkContext
 from pyspark import SQLContext
 
+import warnings
+
 def task2(listings):
-    print "Task 2b"
+    print
+    print "********************* Task 2b *********************"
     print "Distinct values for one column"
     # Ha en for-loop som gjor dette for hver kolonne? Tar bare en naa
-    print "Number of distinct values: ", listings_sample.map(lambda row: row[0]).distinct().count()
-    print
+    # Distinct er casesensitive
+    print "Number of distinct values: ", listings.map(lambda row: row[0]).distinct().count()
 
-    print "Task 2c"
-    cities = listings_sample.map(lambda row: row[15]).distinct()
+    print
+    print "********************* Task 2c *********************"
+    cities = listings.map(lambda row: row[15]).distinct()
     print "Cities count: ", cities.count()
 
     for city in cities.collect():
         print city
 
+    print
+    print "********************* Task 2d *********************"
+
 if __name__ == "__main__":
     sc = SparkContext(appName="AirBnb")
+    sc.setLogLevel("WARN")
 
     listings_textfile = sc.textFile("listings_us.csv")
     listings = listings_textfile.map(lambda x: tuple(x.split('\t')))
     listings_sample = listings.sample(False, 0.1, 7)
-
     # Lagre sample:
     # listings_sample.coalesce(1).saveAsTextFile("sampleFiles.csv")
 
